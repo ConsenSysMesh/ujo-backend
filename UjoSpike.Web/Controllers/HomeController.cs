24e21f83f5631c52998a10c8992abd75cb0607e1
Juan Blanco <juanfranblanco@hotmail.com>
2016-06-19 08:22:22 +0100

    Search example

---
+++ UjoSpike.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UjoSpike.Web.Services;
+
+namespace UjoSpike.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Search(string q = "")
+        {
+            // If blank search, assume they want to search everything
+            if (string.IsNullOrWhiteSpace(q))
+                q = "*";
+
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = new ArtistSearchService().SearchArtist(q).Results
+            };
+        }
+    }
+}
---
+++ UjoSpike.Web/Services/ArtistSearchService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace UjoSpike.Web.Services
+{
+    public class ArtistSearchService
+    {
+        private static SearchServiceClient _searchClient;
+        private static SearchIndexClient _indexClient;
+        private static string IndexName = "namecategoryindex";
+  
+
+        public static string errorMessage;
+
+        static ArtistSearchService()
+        {
+            try
+            {
+                string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+                string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+
+                // Create an HTTP reference to the catalog index
+                _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+                _indexClient = _searchClient.Indexes.GetClient(IndexName);
+
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message.ToString();
+            }
+        }
+
+
+        public DocumentSearchResult SearchArtist(string text)
+        {
+            // Execute search based on query string
+            try
+            {
+                SearchParameters sp = new SearchParameters()
+                {
+                    SearchMode = SearchMode.All,
+                    Top = 20,
+                };
+                return _indexClient.Documents.Search(text, sp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error querying index: {0}\r\n", ex.Message.ToString());
+            }
+            return null;
+        }
+
+        public DocumentSuggestResult Suggest(string searchText, bool fuzzy)
+        {
+            // Execute search based on query string
+            try
+            {
+                SuggestParameters sp = new SuggestParameters()
+                {
+                    UseFuzzyMatching = fuzzy,
+                    Top = 8
+                };
+
+                return _indexClient.Documents.Suggest(searchText, "sg", sp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error querying index: {0}\r\n", ex.Message.ToString());
+            }
+            return null;
+        }
+
+
+
+    }
+}
