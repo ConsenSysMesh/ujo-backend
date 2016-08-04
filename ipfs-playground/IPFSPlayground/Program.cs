4a01b1abe6cd3d755988711e9d4e36edf2e49de2
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-04 19:05:49 +0100

    Restructure

---
+++ ipfs-playground/IPFSPlayground/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ipfs;
+
+namespace IPFSPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessImage().Wait();
+            Console.WriteLine("Done...");
+            Console.ReadLine();
+        }
+
+
+        public static async Task ProcessImage()
+        {
+            using (var ipfs = new IpfsClient("https://ipfs.infura.io:5001"))
+            {
+                //Name of the file to add
+                string fileName = "kids.jpg";
+
+                //Wrap our stream in an IpfsStream, so it has a file name.
+                IpfsStream inputStream = new IpfsStream(fileName, File.OpenRead(fileName));
+
+                MerkleNode node = await ipfs.Add(inputStream);
+
+                Debug.WriteLine(node.Hash.ToString());
+
+                Stream outputStream = await ipfs.Cat(node.Hash.ToString());
+                using (var image = Image.FromStream(outputStream))
+                {
+                    var newImage = ScaleImage(image, 500);
+                    newImage.Save("newKids.jpg", ImageFormat.Jpeg);
+
+                    inputStream = new IpfsStream("kids500", File.OpenRead("newKids.jpg"));
+
+                    node = await ipfs.Add(inputStream);
+
+                    Debug.WriteLine(node.Hash.ToString());
+                }
+
+            }
+        }
+
+        public static System.Drawing.Image ScaleImage(System.Drawing.Image image, int maxHeight)
+        {
+            var ratio = (double)maxHeight / image.Height;
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+            var newImage = new Bitmap(newWidth, newHeight);
+            using (var g = Graphics.FromImage(newImage))
+            {
+                g.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        public static async Task ProcessFile()
+        {
+            using (var ipfs = new IpfsClient())
+            {
+                //Name of the file to add
+                string fileName = "test.txt";
+
+                //Wrap our stream in an IpfsStream, so it has a file name.
+                IpfsStream inputStream = new IpfsStream(fileName, File.OpenRead(fileName));
+
+                MerkleNode node = await ipfs.Add(inputStream);
+
+                Stream outputStream = await ipfs.Cat(node.Hash.ToString());
+
+                using (StreamReader sr = new StreamReader(outputStream)) 
+                {
+                    string contents = sr.ReadToEnd();
+
+                    Console.WriteLine(contents); //Contents of test.txt are printed here!
+                }
+            }
+        }
+    }
+}
--- ipfs-playground/IPFSPlayground/Properties/AssemblyInfo.cs
+++ ipfs-playground/IPFSPlayground/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nethereum.ContractRegistry.Tests")]
+[assembly: AssemblyTitle("IPFSPlayground")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nethereum.ContractRegistry.Tests")]
+[assembly: AssemblyProduct("IPFSPlayground")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d516f08-2b17-4494-a442-8079e3e477ed")]
+[assembly: Guid("e970eca9-e570-4ddd-8356-03b622e33b56")]
 
 // Version information for an assembly consists of the following four values:
 //
