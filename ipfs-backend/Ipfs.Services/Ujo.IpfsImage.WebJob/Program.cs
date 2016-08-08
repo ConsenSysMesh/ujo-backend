10d5e519e491f8a6a76eadebec4ecbf3f736f067
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-08 17:40:27 +0100

    tidying up

--- ipfs-backend/Ipfs.Services/Ujo.IpfsImage.WebJob/Program.cs
+++ ipfs-backend/Ipfs.Services/Ujo.IpfsImage.WebJob/Program.cs
@@ -1,20 +1,4 @@
-﻿//----------------------------------------------------------------------------------
-// Microsoft Developer & Platform Evangelism
-//
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
-// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-//----------------------------------------------------------------------------------
-// The example companies, organizations, products, domain names,
-// e-mail addresses, logos, people, places, and events depicted
-// herein are fictitious.  No association with any real company,
-// organization, product, domain name, email address, logo, person,
-// places, or events is intended or should be inferred.
-//----------------------------------------------------------------------------------
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,27 +10,10 @@ using Microsoft.WindowsAzure.Storage;
 
 namespace Ujo.IpfsImage.WebJob
 {
-    //******************************************************************************************************
-    // This will show you how to perform common scenarios using the Microsoft Azure Table storage service using 
-    // the Microsoft Azure WebJobs SDK. The scenarios covered include reading and writing data to Tables.
-    //
-    // In this sample, the Program class starts the JobHost and creates the demo data. The Functions class
-    // contains methods that will be invoked when messages are placed on the queues and tables, based on the 
-    // attributes in the method headers.
-    //
-    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
-    //
-    // TODO: Open app.config and paste your Storage connection string into the AzureWebJobsDashboard and
-    //      AzureWebJobsStorage connection string settings.      
-    //*****************************************************************************************************
 
     class Program
     {
 
-        // The following code will write a sentence as a message on a queue called textinput.
-        // The SDK will trigger a function called CountAndSplitInWords which is listening on textinput queue.
-        // CountAndSplitInWords will split the sentence into words and store results in Table storage.
-        // 
         static void Main()
         {
             if (!ConfigurationSettings.VerifyConfiguration())
@@ -55,16 +22,11 @@ namespace Ujo.IpfsImage.WebJob
                 return;
             }
 
-
             JobHostConfiguration config = new JobHostConfiguration();
             config.UseTimers();
             var host = new JobHost(config);
             // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
         }
-
-
-
-
     }
 }
