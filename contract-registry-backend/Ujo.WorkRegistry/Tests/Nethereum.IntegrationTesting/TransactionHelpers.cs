df7c87a530a76f7e68b36be607713bdfc6bb2b15
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-18 13:43:44 +0100

    Add a new type of not mining contract deployment

--- contract-registry-backend/Ujo.WorkRegistry/Tests/Nethereum.IntegrationTesting/TransactionHelpers.cs
+++ contract-registry-backend/Ujo.WorkRegistry/Tests/Nethereum.IntegrationTesting/TransactionHelpers.cs
@@ -21,6 +21,13 @@ namespace Nethereum.IntegrationTesting
             return receipt.ContractAddress;
         }
 
+        public async Task<string> DeployContract(Web3.Web3 web3, string addressFrom, string pass, string bytecode, bool mineIt, object[] constructorParameters)
+        {
+            if (mineIt) return await DeployContract(web3, addressFrom, pass, bytecode, constructorParameters);
+            var receipt = await SendTransactionAsync(web3, addressFrom, pass, () => web3.Eth.DeployContract.SendRequestAsync(bytecode, addressFrom, new HexBigInteger(900000), constructorParameters));
+            return receipt.ContractAddress;
+        }
+
         public async Task<string> DeployContract(Web3.Web3 web3, string addressFrom, string pass, string bytecode, bool mineIt)
         {
             if (mineIt) return await DeployContract(web3, addressFrom, pass, bytecode);
