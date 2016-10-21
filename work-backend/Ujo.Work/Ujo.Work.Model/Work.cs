90a7de09fcf6ec11f20ff162400c4e705c4c668d
Juan Blanco <juanfranblanco@hotmail.com>
2016-10-21 14:06:31 +0100

    Extract class work artist, add collections, include check for Address or Name

--- work-backend/Ujo.Work/Ujo.Work.Model/Work.cs
+++ work-backend/Ujo.Work/Ujo.Work.Model/Work.cs
@@ -1,7 +1,43 @@
+using System.Collections.Generic;
+
 namespace Ujo.Work.Model
 {
+    public class WorkArtist
+    {
+        public int Index { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public WorkArtist(int index, string nameOrAddress, string role)
+        {
+            if (IsAddress(nameOrAddress))
+            {
+                this.Address = nameOrAddress;
+            }
+            else
+            {
+                this.Name = nameOrAddress;
+            }
+            this.Index = index;   
+        }
+
+        private bool IsAddress(string nameOrAddress)
+        {
+            return nameOrAddress.StartsWith("0x");
+        }
+
+    }
+
     public class Work
     {
+        public Work()
+        {
+            this.FeaturedArtists = new List<WorkArtist>();
+            this.PerformingArtists = new List<WorkArtist>();
+            this.ContributingArtists = new List<WorkArtist>();
+        }
+
         public string Address { get; set; }
         public string Name { get; set; }
         public string WorkFileIpfsHash { get; set; }
@@ -16,66 +52,11 @@ namespace Ujo.Work.Model
        
         public string Keywords { get; set; }
         public string ByArtist { get; set; }
-        public string FeaturedArtist1 { get; set; }
-        public string FeaturedArtist2 { get; set; }
-        public string FeaturedArtist3 { get; set; }
-        public string FeaturedArtist4 { get; set; }
-        public string FeaturedArtist5 { get; set; }
-        public string FeaturedArtist6 { get; set; }
-        public string FeaturedArtist7 { get; set; }
-        public string FeaturedArtist8 { get; set; }
-        public string FeaturedArtist9 { get; set; }
-        public string FeaturedArtist10 { get; set; }
-        public string FeaturedArtistRole1 { get; set; }
-        public string FeaturedArtistRole2 { get; set; }
-        public string FeaturedArtistRole3 { get; set; }
-        public string FeaturedArtistRole4 { get; set; }
-        public string FeaturedArtistRole5 { get; set; }
-        public string FeaturedArtistRole6 { get; set; }
-        public string FeaturedArtistRole7 { get; set; }
-        public string FeaturedArtistRole8 { get; set; }
-        public string FeaturedArtistRole9 { get; set; }
-        public string FeaturedArtistRole10 { get; set; }
-        public string ContributingArtist1 { get; set; }
-        public string ContributingArtist2 { get; set; }
-        public string ContributingArtist3 { get; set; }
-        public string ContributingArtist4 { get; set; }
-        public string ContributingArtist5 { get; set; }
-        public string ContributingArtist6 { get; set; }
-        public string ContributingArtist7 { get; set; }
-        public string ContributingArtist8 { get; set; }
-        public string ContributingArtist9 { get; set; }
-        public string ContributingArtist10 { get; set; }
-        public string ContributingArtistRole1 { get; set; }
-        public string ContributingArtistRole2 { get; set; }
-        public string ContributingArtistRole3 { get; set; }
-        public string ContributingArtistRole4 { get; set; }
-        public string ContributingArtistRole5 { get; set; }
-        public string ContributingArtistRole6 { get; set; }
-        public string ContributingArtistRole7 { get; set; }
-        public string ContributingArtistRole8 { get; set; }
-        public string ContributingArtistRole9 { get; set; }
-        public string ContributingArtistRole10 { get; set; }
-        public string PerformingArtist1 { get; set; }
-        public string PerformingArtist2 { get; set; }
-        public string PerformingArtist3 { get; set; }
-        public string PerformingArtist4 { get; set; }
-        public string PerformingArtist5 { get; set; }
-        public string PerformingArtist6 { get; set; }
-        public string PerformingArtist7 { get; set; }
-        public string PerformingArtist8 { get; set; }
-        public string PerformingArtist9 { get; set; }
-        public string PerformingArtist10 { get; set; }
-        public string PerformingArtistRole1 { get; set; }
-        public string PerformingArtistRole2 { get; set; }
-        public string PerformingArtistRole3 { get; set; }
-        public string PerformingArtistRole4 { get; set; }
-        public string PerformingArtistRole5 { get; set; }
-        public string PerformingArtistRole6 { get; set; }
-        public string PerformingArtistRole7 { get; set; }
-        public string PerformingArtistRole8 { get; set; }
-        public string PerformingArtistRole9 { get; set; }
-        public string PerformingArtistRole10 { get; set; }
+
+        public List<WorkArtist> FeaturedArtists { get; set; }
+        public List<WorkArtist> ContributingArtists { get; set; }
+        public List<WorkArtist> PerformingArtists { get; set; }
+
         public string Label { get; set; }
         public string Description { get; set; }
         public string Publisher { get; set; }
@@ -84,6 +65,25 @@ namespace Ujo.Work.Model
         public string IsFamilyFriendly { get; set; }
         public string License { get; set; }
         public string IswcCode { get; set; }
+
+        public void AddFeatureArtist(int index, string nameOrAddress, string role)
+        {
+            if(!string.IsNullOrEmpty(nameOrAddress))
+            this.FeaturedArtists.Add(new WorkArtist(index, nameOrAddress, role));
+        }
+
+        public void AddContributingArtist(int index, string nameOrAddress, string role)
+        {
+            if (!string.IsNullOrEmpty(nameOrAddress))
+                this.ContributingArtists.Add(new WorkArtist(index, nameOrAddress, role));
+        }
+
+        public void AddPerformingArtist(int index, string nameOrAddress, string role)
+        {
+            if (!string.IsNullOrEmpty(nameOrAddress))
+                this.PerformingArtists.Add(new WorkArtist(index, nameOrAddress, role));
+        }
+
 
     }
 }
